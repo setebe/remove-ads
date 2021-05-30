9cb19d78b94d42789ce9a4335431ba3cde6c8d17
Ilhomparisi <ilhomparisi@yandex.com>
2021-05-30 13:36:42 +0500

    Word search method changed

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const {Telegraf} = require('telegraf')
 const bot = new Telegraf("1270686685:AAFkDFAM_hog9v1S-FRQA6dKwIb-OCTlgl0")
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.on('text', ctx => {
-let banned = ["bitcoin","invest","sex","cock","suck","dick","tiktok","hook up","my payment","https://t.me/joinchat/","trusted"]
-    if (banned.includes(ctx.message.text.toLowerCase())){
+    let banned = ["bitcoin","invest","sex","cock","suck","dick","tiktok","hook up","my payment","https://t.me/joinchat/","trusted"]
+    if (ctx.message.text.toLowerCase().match(new RegExp(banned.join("|"), "g"))){
+    // if (banned.includes(ctx.message.text.toLowerCase())){
         ctx.deleteMessage()
     }
 })
