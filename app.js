55505341f5e70fafba1f63667804d540ae1d9167
Ilhomparisi <ilhomparisi@yandex.com>
2021-07-14 15:12:07 +0500

    Small fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const {Telegraf} = require('telegraf')
 const bot = new Telegraf("1270686685:AAFkDFAM_hog9v1S-FRQA6dKwIb-OCTlgl0")
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.on('text', ctx => {
-    let banned = ["my bill","debt","my earning","my profit","my money","gamble","trade","my wallet","my profit","Thanks Mrs","bitcoin","invest","sex","cock","suck","dick","tiktok","hook up","my payment","https://t.me/joinchat/","trusted"]
+    let banned = ["my bill","debt","my earning","my profit","my money","gamble","trade","my wallet","my profit","Thanks Mrs","bitcoin","invest","sex","dick","tiktok","hook up","my payment","https://t.me/joinchat/","trusted"]
     if (ctx.message.text.toLowerCase().match(new RegExp(banned.join("|"), "g"))){
     // if (banned.includes(ctx.message.text.toLowerCase())){
         ctx.deleteMessage()
